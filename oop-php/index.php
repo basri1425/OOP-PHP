303fcb81ebb7f3723803442db0a8622e2f8f060f
Basri <58840135+basri1425@users.noreply.github.com>
2020-06-26 07:23:38 +0700

    update

--- oop-php/index.php
+++ oop-php/index.php
@@ -11,11 +11,18 @@ echo $sheep->legs."<br>"; // 2
 echo $sheep->cold_blooded."<br>"; // false
 
 // NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
-
+echo "<br>";
 // index.php
 $sungokong = new Ape("kera sakti");
+echo $sungokong->name."<br>"; // "kera sakti"
+echo $sungokong->legs."<br>"; // 2
+echo $sungokong->cold_blooded; // false
 $sungokong->yell(); // "Auooo"
 
+echo "<br>";
 $kodok = new Frog("buduk");
+echo $kodok->name."<br>"; // "buduk"
+echo $kodok->legs."<br>"; // 2
+echo $kodok->cold_blooded; // false
 $kodok->jump() ; // "hop hop"
  ?>
